a05fc8f2efbe5a5218d8dd903cc7128e42fc5370
cohenedavid <cohenedavid1018@gmail.com>
2022-01-18 00:23:44 -0300

    Strings: rebanadas

---
+++ Strings_prueba_1.py
@@ -0,0 +1,20 @@
+word = "Python"
+
+rebanada1 = word[0:6]   # desde el caracter 0 (incluido) hasta el 6 (excluido)
+rebanada2 = word[0:]    # desde el caracter 0 (incluido) hasta el ultimo de la cadena
+
+rebanada3 = word[0:3]   # desde el caracter 0 (incluido) hasta el 3 (excluido)
+
+print(rebanada1)
+print(rebanada3)
+
+print(word + ' Tutorial') # imprimir variable concatenado a un literal
+
+
+"""
+ +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1
+"""
