11dc80769d2753b00109f2321b03ec88861ffbf0
cohenedavid <cohenedavid1018@gmail.com>
2022-01-18 14:26:57 -0300

    Agrego tercer commit de Listas

--- Tipos_de_datos_compuestos_Listas.py
+++ Tipos_de_datos_compuestos_Listas.py
@@ -25,7 +25,7 @@ numneros += [25, -10, 350]
 print(numneros)
 
 # Las listas son mutables, podemos modificarlas
-numeros_impares = [1, 3, 5, 7, 9] # ERROR -> El 9 nueve no es un numero par
+numeros_impares = [1, 3, 5, 7, 9]                       # ERROR -> El 9 nueve no es un numero par
 
 numeros_impares[4] = 11
 print(numeros_impares)
@@ -37,8 +37,21 @@ print(numeros_impares)
 # Reemplazar valores intermedios dentro de una lista mediante rebanadas
 abecedario = ['a','b','c','d','e','l','m','n','i','j']  # Acá los caracteres l,m,n están mal ubicados
 print(abecedario)                                       # Debemos reemplazarlos por f,g,h que están
-# Salida -> ['a', 'b', 'c', 'd', 'e', 'l', 'm', 'n', 'i', 'j']    # ubicados en las posiciones 5,6,7
-abecedario[5:8] = ['f','g','h'] # lo reemplazamos empleando rebanadas
-print(abecedario) # Vemos que ahora ha quedado correctamente
-# Salida -> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+                                                        # ubicados en las posiciones 5,6,7
+# Salida --> ['a', 'b', 'c', 'd', 'e', 'l', 'm', 'n', 'i', 'j']    
+abecedario[5:8] = ['f','g','h']                         # lo reemplazamos empleando rebanadas
+print(abecedario)                                       # Vemos que ahora ha quedado correctamente
+# Salida --> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+# Obtener el largo de una lista con la funcióm len()
+print(len(abecedario))
+
+# Guardar listas dentro de otra lista: Anidar listas
+vocales_abiertas = ['a', 'o', 'e']
+vocales_cerradas = ['i', 'u']
+vocales = [vocales_abiertas, vocales_cerradas]
+print(vocales)
+# Salida --> [['a', 'o', 'e'], ['i', 'u']]
+print(vocales[0]) # Vocales tiene dos índeces: [0] y [1], siendo [0] la lista de vocales_abiertas
+# Salida --> ['a', 'o', 'e']
 
